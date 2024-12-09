4e2f0af97a1552e57fe91aabff62a38f2b3f4f51
_apriannr <apriannurrohman@gmail.com>
2024-12-09 07:06:03 +0700

    hide file upload

--- backend/index.js
+++ backend/index.js
@@ -27,27 +27,27 @@ app.get("/", (req, res) => {
 });
 
 // Image Storage Engine
-const storage = multer.diskStorage({
-  destination: "./upload/images",
-  filename: (req, file, cb) => {
-    return cb(
-      null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "./upload/images",
+//   filename: (req, file, cb) => {
+//     return cb(
+//       null,
+//       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+//     );
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 // Creating Upload Endpoint for Images
-app.use("/images", express.static("upload/images"));
+// app.use("/images", express.static("upload/images"));
 
-app.post("/upload", upload.single("product"), (req, res) => {
-  res.json({
-    success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
-  });
-});
+// app.post("/upload", upload.single("product"), (req, res) => {
+//   res.json({
+//     success: 1,
+//     image_url: `http://localhost:${port}/images/${req.file.filename}`,
+//   });
+// });
 
 // Creating API for Deleting Products
 const Product = mongoose.model("Product", {
